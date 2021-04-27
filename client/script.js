867f68ae5ec8f0d10196d5806cdfdf8d5e68ad60
Jordan-Madden <jordanmadden285@gmail.com>
2021-04-26 23:41:07 -0500

    Updating ode

--- client/script.js
+++ client/script.js
@@ -1,4 +1,4 @@
-function createPatientCards(){
+function createPatientCards(patient){
     var patientDataDiv = document.createElement("DIV");
     patientDataDiv.classList.add("patient_data");
 
@@ -14,6 +14,34 @@ function createPatientCards(){
     var displayDiv = document.createElement("DIV");
     displayDiv.classList.add("display");
 
+    var firstNameDiv = document.createElement("DIV");
+    firstNameDiv.classList.add("first_name");
+    var firstNameSpan1 = document.createElement("SPAN");
+    firstNameSpan1.innerHTML = "First Name";
+    var firstNameSpan2 = document.createElement("SPAN");
+    firstNameSpan2.innerHTML = "John";
+    firstNameDiv.append(firstNameSpan1);
+    firstNameDiv.append(firstNameSpan2);
+
+    var lastNameDiv = document.createElement("DIV");
+    lastNameDiv.classList.add("last_name");
+    var lastNameSpan1 = document.createElement("SPAN");
+    lastNameSpan1.innerHTML = "Last Name";
+    var lastNameSpan2 = document.createElement("SPAN");
+    lastNameSpan2.innerHTML = "Doe";
+    lastNameDiv.append(lastNameSpan1);
+    lastNameDiv.append(lastNameSpan2);
+
+    var positionDiv = document.createElement("DIV");
+    positionDiv.classList.add("position");
+    var positionSpan1 = document.createElement("SPAN");
+    positionSpan1.innerHTML = "Position";
+    var positionSpan2 = document.createElement("SPAN");
+    positionSpan2.innerHTML = 350;
+    positionDiv.append(positionSpan1);
+    positionDiv.append(positionSpan2);
+    
+
     var patientIdDiv = document.createElement("DIV");
     patientIdDiv.classList.add("patient_id");
     var patientIdSpan1 = document.createElement("SPAN");
@@ -23,27 +51,10 @@ function createPatientCards(){
     patientIdDiv.append(patientIdSpan1);
     patientIdDiv.append(patientIdSpan2);
 
-    var positionDiv = document.createElement("DIV");
-    positionDiv.classList.add("position");
-    var positionSpan1 = document.createElement("SPAN");
-    positionSpan1.innerHTML = "Position";
-    var positionSpan2 = document.createElement("SPAN");
-    positionSpan2.innerHTML = 150;
-    positionDiv.append(positionSpan1);
-    positionDiv.append(positionSpan2);
-
-    var tempDiv = document.createElement("DIV");
-    tempDiv.classList.add("temperature");
-    var tempSpan1 = document.createElement("SPAN");
-    tempSpan1.innerHTML = "Temperature";
-    var tempSpan2 = document.createElement("SPAN");
-    tempSpan2.innerHTML = 36;
-    tempDiv.append(tempSpan1);
-    tempDiv.append(tempSpan2);
-
-    displayDiv.append(patientIdDiv);
-    displayDiv.append(positionDiv);
-    displayDiv.append(tempDiv);          
+    displayDiv.append(firstNameDiv);
+    displayDiv.append(lastNameDiv);   
+    displayDiv.append(positionDiv); 
+    displayDiv.append(patientIdDiv);      
 
     patientDataDiv.append(patientImgDiv);
     patientDataDiv.append(displayDiv);
@@ -52,23 +63,20 @@ function createPatientCards(){
 }
 
 function getPatientData(){
-    return fetch("http://192.168.1.7:5000/data")
-        .then(res => res.json)
-        .then(json => json);
+    return fetch("http://192.168.1.11:5000/api/patient").then(res => res.json()).then(json => json)
 }
 
 async function displayPatientData(){
-    // let patients = await getPatientData();
-    // console.log(patients);
-    let i = 0;
-    while(i < 5){
+    let patients = await getPatientData();
+    console.log(patients);
+
+    patients.forEach(patient => {
         var content = document.querySelector(".content");
-        content.append(createPatientCards());
-        i++;
-    }
+        content.append(createPatientCards(patient));
+    });
 }
 
 window.onload = function(){
-    console.log("[INFO] Working...");
+    //console.log("[INFO] Working...");
     displayPatientData();
 }
