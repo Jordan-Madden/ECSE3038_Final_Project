a5e8a176bd33055f5c57d43c722263b0ad7a2ad8
Jordan-Madden <jordanmadden285@gmail.com>
2021-04-28 11:29:59 -0500

    Completed embedded software

--- embedded/embedded.ino
+++ embedded/embedded.ino
@@ -13,9 +13,12 @@ unsigned long timer = 0;
 float timeStep = 0.01;
 
 // Pitch, Roll and Yaw values
-float pitch = 0.0;
-float roll = 0.0;
-float yaw = 0.0;
+int pitch = 0.0;
+int roll = 0.0;
+int yaw = 0.0;
+
+// ESP MAC Address
+String espMacAddress;
 
 void espSetup(){
   String networkName = "FLOW-WiFi";
@@ -25,7 +28,7 @@ void espSetup(){
   sendData("AT+RST\r\n", 10000, DEBUG);
   
   // Configure ESP to operate as client
-//  sendData("AT+CWMODE=3\r\n", 10000, DEBUG);
+  sendData("AT+CWMODE=3\r\n", 10000, DEBUG);
 
   // List access points
 //  sendData("AT+CWLAP\r\n", 10000, DEBUG);
@@ -34,51 +37,13 @@ void espSetup(){
   sendData("AT+CWJAP=\"FLOW-WiFi\",\"Honeymad5\"\r\n", 5000, DEBUG);
 
   // Verify that access point has been joined
-//  sendData("AT+CIFSR\r\n", 3000, DEBUG);  
+  //sendData("AT+CIFSR\r\n", 3000, DEBUG);  
 }
 
-void gyroscopeSetup(){
-  while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
-  {
-    Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
-    delay(500);
-  }
-  
-  // Calibrate gyroscope. The calibration must be at rest.
-  // If you don't want calibrate, comment this line.
-  mpu.calibrateGyro();
-
-  // Set threshold sensivty. Default 3.
-  // If you don't want use threshold, comment this line or set 0.
-  mpu.setThreshold(0);  
-
-  Serial.println("MPU6050 Ready!");
-  delay(1500);
-}
-
-void readGyroscope(){
-  timer = millis();
-  
-  // Read normalized values
-  Vector norm = mpu.readNormalizeGyro();
-
-  // Calculate Pitch, Roll and Yaw
-  pitch = pitch + norm.YAxis * timeStep;
-  roll = roll + norm.XAxis * timeStep;
-  yaw = yaw + norm.ZAxis * timeStep;  
-
-  // Wait to full timeStep period
-  delay((timeStep*1000) - (millis() - timer));
-}
-
-float getTemperature(){
-  float voltage, temp;
-
-  // Read temperature from the LM35 sensor
-  voltage = analogRead(LM35) * (5.0/1023.0);
-  temp = 100 * voltage;
-
-  return temp;
+String getMacAddress(){
+  String response = "";
+  response = sendData("AT+CIPSTAMAC?\r\n\r\n", 3000, false);  
+  return response.substring(42, 59);
 }
 
 String sendData(String command, const int timeout, boolean debug) {
@@ -107,9 +72,69 @@ String sendData(String command, const int timeout, boolean debug) {
     return response;
 }
 
+void gyroscopeSetup(){
+  // Initialize MPU6050
+  Serial.println("Initialize MPU6050");
+  while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
+  {
+    Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
+    delay(500);
+  }
+  
+  // If you want, you can set gyroscope offsets
+  mpu.setGyroOffsetX(138);
+  mpu.setGyroOffsetY(80);
+  mpu.setGyroOffsetZ(9);
+  
+  // Calibrate gyroscope. The calibration must be at rest.
+  // If you don't want calibrate, comment this line.
+  mpu.calibrateGyro();
+
+  // Set threshold sensivty. Default 3.
+  // If you don't want use threshold, comment this line or set 0.
+  mpu.setThreshold(1);
+}
+
+int readGyroscope(){  
+  Vector rawGyro = mpu.readRawGyro();
+  Vector normGyro = mpu.readNormalizeGyro();
+
+//  Serial.print(" Xraw = ");
+//  Serial.print(rawGyro.XAxis);
+//  Serial.print(" Yraw = ");
+//  Serial.print(rawGyro.YAxis);
+//  Serial.print(" Zraw = ");
+//  Serial.println(rawGyro.ZAxis);
+//
+//  Serial.print(" Xnorm = ");
+//  Serial.print(normGyro.XAxis);
+//  Serial.print(" Ynorm = ");
+//  Serial.print(normGyro.YAxis);
+//  Serial.print(" Znorm = ");
+//  Serial.println(normGyro.ZAxis);
+
+  return (int)rawGyro.YAxis;
+}
+
+int getTemperature(int testing){
+  float voltage, temp;
+
+  if (testing == 0){
+    // Read temperature from the LM35 sensor
+    voltage = analogRead(LM35) * (5.0/1023.0);
+    temp = 100 * voltage;
+  }
+  else{
+    // Generate random test data
+    temp = random(30, 41);
+  }  
+
+  return (int)temp;
+}
+
 String generatePostRequest(String route, String portNumber, int cLength, String pData) {
   String requestType = "POST /" + route + " HTTP/1.1\r\n";
-  String hostInfo = "Host: 192.168.1.7:" + portNumber + "\r\n";
+  String hostInfo = "Host: 192.168.1.11:" + portNumber + "\r\n";
   String contentType = "Content-Type: application/json\r\n";
   String contentLength = "Content-Length: " + String(cLength) + "\r\n\r\n";
   String postData = pData + "\r\n\r\n";
@@ -123,8 +148,8 @@ String generateCIPSend(int requestLength){
   return cipSend;
 }
 
-String generatePost(int patient_id, float pos, int temp){
-  String post = "{\"patient_id\":" +String(patient_id)+ ", \"position\":" +String(pos)+ ", \"temperature\":"+String(temp)+"}\r\n\r\n";
+String generatePost(String patient_id, float pos, int temp){
+  String post = "{\"patient_id\": \""+patient_id+ "\", \"position\": "+String(pos)+ ", \"temperature\": "+String(temp)+"}\r\n\r\n";
   
   return post;
 }
@@ -133,35 +158,37 @@ void setup() {
   Serial.begin(115200);
   Serial1.begin(115200);
 
-  // Setup the gyroscope
-  //gyroscopeSetup();
+  Serial.println("################################################");
+  Serial.println("                   START SETUP                  ");
+  Serial.println("################################################");
 
-  sendData("AT+RST", 5000, DEBUG);
+  // Setup the gyroscope
+  gyroscopeSetup();
 
   // Setup the ESP8266
   espSetup();
+
+  // Get the MAC address of the ESP
+  espMacAddress = getMacAddress();
+  Serial.print("MAC Address: "); Serial.println(espMacAddress);
+
+  Serial.println("################################################");
+  Serial.println("                     END SETUP                  ");
+  Serial.println("################################################");
 }
 
 void loop() {  
-  sendData("AT+CIPSTART=\"TCP\",\"192.168.1.7\",5000\r\n", 1500, DEBUG);
+  int temp;
+  int pos;
 
-  String postData = "{\"patient_id\": \"156\", \"position\": 273, \"temperature\": 35}";
-  String postRequest = generatePostRequest("data", "5000", postData.length(), postData);  
+  pos = readGyroscope();
+  temp = getTemperature(0);
+
+  String postData = generatePost(espMacAddress, pos, temp);
+  String postRequest = generatePostRequest("api/record", "5000", postData.length(), postData);  
   String CIPSend = generateCIPSend(postRequest.length());
 
-  Serial.println(postData);
-
+  sendData("AT+CIPSTART=\"TCP\",\"192.168.1.11\",5000\r\n", 3000, DEBUG);
   sendData(CIPSend, 1000, DEBUG);
   sendData(postRequest, 5000, DEBUG);
-
-//  readGyroscope();
-//
-//  int temp = getTemperature();
-//
-//  Serial.print("Yaw: "); Serial.print(yaw);
-//  Serial.print("\tPitch: "); Serial.print(pitch);
-//  Serial.print("\tRoll: "); Serial.println(roll);
-//  Serial.print("Temperature: "); Serial.println(temp);
-
-  delay(500);
 }
